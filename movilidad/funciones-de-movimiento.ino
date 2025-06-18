baa07761e7a51cd1fb0abb2abef6aad85692d606
vickyT049 <victoriasoledadtorrez@gmail.com>
2025-06-17 21:01:22 -0300

    Update funciones-de-movimiento.ino
     Descripción sencilla y detallada de lo que hace este código:
    Este código está diseñado para mover un robot en una dirección específica (eje), una cierta distancia (en centímetros), calculando el tiempo necesario para que los motores lo logren. Se utilizan tres motores (motor1, motor2, motor3) controlados por una función mover(). El movimiento depende del eje seleccionado:
    
    Eje 3: Movimiento hacia adelante o atrás.
    
    Eje 1: Movimiento lateral (izquierda/derecha).
    
    Eje 2: Movimiento en diagonal.

--- movilidad/funciones-de-movimiento.ino
+++ movilidad/funciones-de-movimiento.ino
@@ -1,49 +1,40 @@
 #include <Arduino.h>
 #include <zirconLib.h>
 int sentido=1;
-int eje; 
-int mover1 (int eje){
-  motor1(100,1);
-  motor2(100,1);
-  delay(5000);
-  motor1(0,0);
-  motor2(0,0);
-}
-int mover2 (int eje){
-  motor1(100,1);
-  motor3(100,1);
-  delay(5000);
-  motor1(0,0);
-  motor3(0,0);
-}
-int mover3 (int eje){
-  motor2(100,1);
-  motor3(100,1);
-  delay(5000);
-  motor2(0,0);
-  motor3(0,0);
-}
-float angulo;
-
-
-float girar (float angulo) {
-  
-  
-  float tiempo = angulo * 0.075 / 90;
-  
-  motor1(100, 1);
+float tiempo;
+float asda
+float mover (float centrimetros, int eje) {
+  float tiempo = centimetros  s *1000 / 59,7
+  if (eje==3){
+  motor1(100, 0);
   motor2(100, 1);
-  motor3(100, 1);
   delay(tiempo);
-  motor1(0, 1);
-  motor2(0,1);
-  motor3(0,1);
-  delay(2000);
+  motor1(0, 0);
+  motor2(0, 1);
+  }
+  if(eje==1){
+    motor3(100, 0);
+    motor2(100, 1);
+    delay(tiempo);
+    motor3(0, 0);
+    motor2(0, 1);
+  }
+  if(eje==2){
+    motor3(100, 0);
+    motor1(100, 1);
+    delay(tiempo);
+    motor3(0, 0);
+    motor1(0, 1);
+  }
   return tiempo;
 }
+
 void setup(void)
 {
-  Serial.begin(9600);
   InitializeZircon();
-
+}
+void loop(void)
+{  
+mover(80,eje3);
+ 
 }
